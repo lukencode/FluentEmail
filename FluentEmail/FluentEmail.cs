7fa811bef813de3124d35101fb5bb00ec7da5e48
unknown <luke@.(none)>
2010-04-10 12:41:26 +1000

    first commit?
    Signed-off-by:  <luke>

---
+++ FluentEmail/FluentEmail.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+
+namespace FluentEmail
+{
+    public class Email
+    {
+        public MailMessage Message { get; set; }
+        public Exception Error { get; set; }
+        public bool HasError
+        {
+            get
+            {
+                return Error != null;
+            }
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+
+            }
+        }
+
+        public static Email New { get { return new Email(); } }
+
+        private Email()
+        {
+            Message = new MailMessage() { IsBodyHtml = true };
+        }
+
+        public Email To(string emailAddress, string name = "")
+        {
+            Message.To.Add(new MailAddress(emailAddress, name));
+            return this;
+        }
+
+        public Email From(string emailAddress, string name = "")
+        {
+            Message.From = new MailAddress(emailAddress, name);
+            return this;
+        }
+
+        public Email Subject(string subject)
+        {
+            Message.Subject = subject;
+            return this;
+        }
+
+        public Email Body(string body)
+        {
+            Message.Body = body;
+            return this;
+        }
+
+        public Email Send()
+        {
+            try
+            {
+                var client = new SmtpClient();
+                client.Send(Message);
+            }
+            catch (Exception ex)
+            {
+                Error = ex;
+            }
+
+            return this;
+        }
+    }
+}
---
+++ FluentEmailTests/FluentEmailTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentEmail;
+
+namespace FluentEmailTests
+{
+    [TestClass]
+    public class FluentEmailTests
+    {
+        [TestMethod]
+        public void To_Address_Is_Set()
+        {
+            string toEmail = "bob@test.com";
+
+            var email = Email.New
+                .To(toEmail);
+
+            Assert.AreEqual(toEmail, email.Message.To[0].Address);
+        }
+
+        [TestMethod]
+        public void From_Address_Is_Set()
+        {
+            string fromEmail = "johno@test.com";
+
+            var email = Email.New
+                .From(fromEmail);
+
+            Assert.AreEqual(fromEmail, email.Message.From.Address);
+        }
+
+        [TestMethod]
+        public void Subject_Is_Set()
+        {
+            string subject = "sup dawg";
+
+            var email = Email.New
+                .Subject(subject);
+
+            Assert.AreEqual(subject, email.Message.Subject);
+        }
+
+        [TestMethod]
+        public void Body_Is_Set()
+        {
+            string body = "what be the hipitity hap?";
+
+            var email = Email.New
+                .Body(body);
+
+            Assert.AreEqual(body, email.Message.Body);
+        }
+
+        [TestMethod]
+        public void Can_Add_Multiple_Recipients()
+        {
+            string toEmail1 = "bob@test.com";
+            string toEmail2 = "ratface@test.com";
+
+            var email = Email.New
+                .To(toEmail1)
+                .To(toEmail2);
+
+            Assert.AreEqual(2, email.Message.To.Count);
+        }
+
+        public void Is_Valid_With_Properties_Set()
+        {        
+            string toEmail = "bob@test.com";   
+            string fromEmail = "johno@test.com"; 
+            string subject = "sup dawg";
+            string body = "what be the hipitity hap?";
+
+            var email = Email.New
+                .To(toEmail)
+                .Subject(subject)
+                .Body(body)
+                .From(fromEmail);
+        }
+    }
+}
