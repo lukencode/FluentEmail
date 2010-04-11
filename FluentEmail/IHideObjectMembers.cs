48d7e70268d41d0ee71cce11d5ed5778b38e7283
Luke Lowrey <lukenlowrey@gmail.com>
2010-04-11 14:09:35 +1000

    renamed test project

---
+++ FluentEmail/IHideObjectMembers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace FluentEmail
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public interface IHideObjectMembers
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        Type GetType();
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        int GetHashCode();
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        string ToString();
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        bool Equals(object obj);
+    }
+
+}
