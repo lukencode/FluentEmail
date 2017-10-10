dbbe6243ee94cbdda0f15014cbab6edb8e5d56ca
Ben Cull <bjcull@gmail.com>
2017-10-10 13:32:41 +0200

    Removed RestSharp from Mailgun sender

---
+++ src/Senders/FluentEmail.Mailgun/HttpHelpers/HttpFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FluentEmail.Mailgun.HttpHelpers
+{
+    public class HttpFile
+    {
+        public string ParameterName { get; set; }
+        public string Filename { get; set; }
+        public Stream Data { get; set; }
+        public string ContentType { get; set; }
+    }
+}
