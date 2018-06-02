a4e637d2f1112de307710838b5e818e412eaaa4e
Luke Lowrey <lukenlowrey@gmail.com>
2018-06-02 20:46:05 +1000

    wip prototype for better dep injection

---
+++ sample/SampleWeb/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentEmail.Core;
+using Microsoft.AspNetCore.Mvc;
+using SampleWeb.Models;
+
+namespace SampleWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index([FromServices]IFluentEmail email)
+        {
+            var model = new
+            {
+                Name = "test name"
+            };
+
+            await email
+                .To("test@test.test")
+                .Subject("test email subject")
+                .UsingTemplate(@"hi @Model.Name this is a razor template @(5 + 5)!", model)
+                .SendAsync();
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/Senders/FluentEmail.Smtp/FluentEmailSmtpBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using FluentEmail.Core.Interfaces;
+using FluentEmail.Smtp;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class FluentEmailSmtpBuilderExtensions
+    {
+        public static FluentEmailServicesBuilder AddSmtpSender(this FluentEmailServicesBuilder builder, string host, int port)
+        {
+            var smtpClient = new SmtpClient(host, port);
+            builder.Services.TryAdd(ServiceDescriptor.Transient<ISender>(x => new SmtpSender(smtpClient)));
+            return builder;
+        }
+
+        public static FluentEmailServicesBuilder AddSmtpSender(this FluentEmailServicesBuilder builder, SmtpClient smtpClient)
+        {
+            builder.Services.TryAdd(ServiceDescriptor.Transient<ISender>(x => new SmtpSender(smtpClient)));
+            return builder;
+        }
+
+        public static FluentEmailServicesBuilder AddSmtpSender(this FluentEmailServicesBuilder builder, Func<SmtpClient> clientFactory)
+        {
+            builder.Services.TryAdd(ServiceDescriptor.Transient<ISender>(x => new SmtpSender(clientFactory)));
+            return builder;
+        }
+    }
+}
