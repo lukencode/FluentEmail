baf2a3925a0cbddc7f1deb105cdbb4c59a4e2012
Barry Mooring <barry@codingbadger.com>
2018-09-06 12:09:30 +0100

    #119 Add Mailgun region to middleware extension method

--- src/Senders/FluentEmail.Mailgun/FluentEmailMailgunBuilderExtensions.cs
+++ src/Senders/FluentEmail.Mailgun/FluentEmailMailgunBuilderExtensions.cs
@@ -6,9 +6,9 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class FluentEmailMailgunBuilderExtensions
     {
-        public static FluentEmailServicesBuilder AddMailGunSender(this FluentEmailServicesBuilder builder, string domainName, string apiKey)
+        public static FluentEmailServicesBuilder AddMailGunSender(this FluentEmailServicesBuilder builder, string domainName, string apiKey, MailGunRegion mailGunRegion = MailGunRegion.USA)
         {
-            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(x => new MailgunSender(domainName, apiKey)));
+            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(x => new MailgunSender(domainName, apiKey, mailGunRegion)));
             return builder;
         }
     }
