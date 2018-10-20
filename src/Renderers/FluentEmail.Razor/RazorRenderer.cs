3e147041d99b51f343fb1bded2c2f6501d11a220
Ben Cull <bjcull@gmail.com>
2018-10-20 17:41:22 +1100

    Updated Razor project to make use of razorlight 2.0.0-beta1

--- src/Renderers/FluentEmail.Razor/RazorRenderer.cs
+++ src/Renderers/FluentEmail.Razor/RazorRenderer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using FluentEmail.Core.Interfaces;
 using RazorLight;
@@ -8,16 +10,29 @@ namespace FluentEmail.Razor
     public class RazorRenderer : ITemplateRenderer
     {
         public async Task<string> ParseAsync<T>(string template, T model, bool isHtml = true)
-        {
-            var project = new InMemoryRazorLightProject();
-            var engine = new EngineFactory().Create(project);
-            
-            return await engine.CompileRenderAsync<T>(Guid.NewGuid().ToString(), template, model);
+        {            
+            var engine = new RazorLightEngineBuilder()
+                .UseMemoryCachingProvider()
+                .Build();            
+
+            return await engine.CompileRenderAsync<T>(GetHashString(template), template, model);
         }
 
         string ITemplateRenderer.Parse<T>(string template, T model, bool isHtml)
         {
             return ParseAsync(template, model, isHtml).GetAwaiter().GetResult();
+        }
+
+        public static string GetHashString(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+            var hashbytes = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(inputString));
+            foreach (byte b in hashbytes)
+            {
+                sb.Append(b.ToString("X2"));
+            }
+
+            return sb.ToString();
         }
     }
 }
