e45b9349a84e40221560609877e6bb00c5bdd86d
Matt Rutledge <mtrutledge@gmail.com>
2019-04-26 13:22:53 -0600

    Add MailKit Smtp Sender

---
+++ src/Senders/FluentEmail.MailKit/SmtpClientOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FluentEmail.MailKitSmtp
+{
+    public class SmtpClientOptions
+    {
+        public string Server { get; set; } = string.Empty;
+        public int Port { get; set; } = 25;
+        public string User { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool UseSsl { get; set; } = false;
+        public bool RequiresAuthentication { get; set; } = false;
+        public string PreferredEncoding { get; set; } = string.Empty;
+    }
+}
