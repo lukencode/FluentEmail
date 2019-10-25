01d082259d5b94671130bfa9506568d5c82344b4
anaydenov <mail.infokrat@yandex.ru>
2019-10-25 16:54:18 +0300

    Add ElasticEmail Sender

---
+++ src/Senders/FluentEmail.ElasticEmail/FluentEmailElasticEmailBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FluentEmail.Core.Interfaces;
+using FluentEmail.ElasticEmail;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class FluentEmailElasticEmailBuilderExtensions
+    {
+        public static FluentEmailServicesBuilder AddElasticEmailSender(this FluentEmailServicesBuilder builder, string apiKey, bool useSubAccount = false)
+        {
+            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(x => new ElasticEmailSender(apiKey, useSubAccount: useSubAccount)));
+            return builder;
+        }
+    }
+}
