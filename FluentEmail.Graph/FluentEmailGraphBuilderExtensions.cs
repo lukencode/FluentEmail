caeb1d60066bf04ef30011222986e6ae0f4d191a
Matt Goldman <MattGoldman@ssw.com.au>
2020-06-14 15:16:38 +1000

    Added graph sender class library

---
+++ FluentEmail.Graph/FluentEmailGraphBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FluentEmail.Core.Interfaces;
+using FluentEmail.Graph;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class FluentEmailGraphBuilderExtensions
+    {
+        public static FluentEmailServicesBuilder AddGraphSender(this FluentEmailServicesBuilder builder, string GraphEmailAppId, string GraphEmailTenantId, string GraphEmailSecret)
+        {
+            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(x => new GraphSender(GraphEmailAppId, GraphEmailTenantId, GraphEmailSecret)));
+            return builder;
+        }
+    }
+}
---
+++ FluentEmail.Graph/GraphSender.cs
@@ -0,0 +1,32 @@
+﻿using FluentEmail.Core;
+using FluentEmail.Core.Interfaces;
+using FluentEmail.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FluentEmail.Graph
+{
+    public class GraphSender : ISender
+    {
+        public GraphSender(
+            string GraphEmailAppId,
+            string GraphEmailTenantId,
+            string GraphEmailSecret)
+        {
+
+        }
+
+        public SendResponse Send(IFluentEmail email, CancellationToken? token = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SendResponse> SendAsync(IFluentEmail email, CancellationToken? token = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
