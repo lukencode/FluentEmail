5292e0cfb87b6d1e1e25609aacd3810b013d003f
Hugo Aponte <huaponte@microsoft.com>
2021-01-04 16:55:40 -0800

    Upgrading RazorLight to rc 3

--- src/Renderers/FluentEmail.Razor/RazorRenderer.cs
+++ src/Renderers/FluentEmail.Razor/RazorRenderer.cs
@@ -10,61 +10,54 @@ using System.Threading.Tasks;
 namespace FluentEmail.Razor
 {
 	public class RazorRenderer : ITemplateRenderer
-    {
-	    private readonly RazorLightEngine _engine;
+	{
+		private readonly RazorLightEngine _engine;
 
-	    public RazorRenderer()
-	    {
-		    _engine = new RazorLightEngineBuilder()
-			    .UseMemoryCachingProvider()
-			    .Build();      
-	    }
-
-	    public RazorRenderer(string root)
-	    {
-		    _engine = new RazorLightEngineBuilder()
-			    .UseFilesystemProject(root ?? Directory.GetCurrentDirectory())
-			    .UseMemoryCachingProvider()
-			    .Build();      
-	    }
-
-	    public RazorRenderer(RazorLightProject project)
-	    {
-		    _engine = new RazorLightEngineBuilder()
-			    .UseProject(project)
+		public RazorRenderer(string root = null)
+		{
+			_engine = new RazorLightEngineBuilder()
+				.UseFileSystemProject(root ?? Directory.GetCurrentDirectory())
 				.UseMemoryCachingProvider()
-			    .Build();      
-	    }
+				.Build();
+		}
 
-	    public RazorRenderer(Type embeddedResRootType)
-	    {
-		    _engine = new RazorLightEngineBuilder()
-			    .UseEmbeddedResourcesProject(embeddedResRootType)
-			    .UseMemoryCachingProvider()
-			    .Build();      
-	    }
+		public RazorRenderer(RazorLightProject project)
+		{
+			_engine = new RazorLightEngineBuilder()
+				.UseProject(project)
+				.UseMemoryCachingProvider()
+				.Build();
+		}
 
-	    public async Task<string> ParseAsync<T>(string template, T model, bool isHtml = true)
-	    {            
-		    dynamic viewBag = (model as IViewBagModel)?.ViewBag;
-		    return await _engine.CompileRenderAsync<T>(GetHashString(template), template, model, viewBag);
-	    }
+		public RazorRenderer(Type embeddedResRootType)
+		{
+			_engine = new RazorLightEngineBuilder()
+				.UseEmbeddedResourcesProject(embeddedResRootType)
+				.UseMemoryCachingProvider()
+				.Build();
+		}
 
-	    string ITemplateRenderer.Parse<T>(string template, T model, bool isHtml)
-	    {
-		    return ParseAsync(template, model, isHtml).GetAwaiter().GetResult();
-	    }
+		public async Task<string> ParseAsync<T>(string template, T model, bool isHtml = true)
+		{
+			dynamic viewBag = (model as IViewBagModel)?.ViewBag;
+			return await _engine.CompileRenderStringAsync<T>(GetHashString(template), template, model, viewBag);
+		}
 
-	    public static string GetHashString(string inputString)
-	    {
-		    var sb = new StringBuilder();
-		    var hashbytes = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(inputString));
-		    foreach (byte b in hashbytes)
-		    {
-			    sb.Append(b.ToString("X2"));
-		    }
+		string ITemplateRenderer.Parse<T>(string template, T model, bool isHtml)
+		{
+			return ParseAsync(template, model, isHtml).GetAwaiter().GetResult();
+		}
 
-		    return sb.ToString();
-	    }
-    }
+		public static string GetHashString(string inputString)
+		{
+			var sb = new StringBuilder();
+			var hashbytes = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(inputString));
+			foreach (byte b in hashbytes)
+			{
+				sb.Append(b.ToString("X2"));
+			}
+
+			return sb.ToString();
+		}
+	}
 }
