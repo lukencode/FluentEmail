4078a67f5dd22e5e8033bf4fea602ed3ef57e7ed
David De Sloovere <david.desloovere@outlook.com>
2021-02-22 15:42:28 +0100

    Use class as options for GraphSender

--- src/Senders/FluentEmail.Graph/FluentEmailGraphBuilderExtensions.cs
+++ src/Senders/FluentEmail.Graph/FluentEmailGraphBuilderExtensions.cs
@@ -4,17 +4,34 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-    public static class FluentEmailGraphBuilderExtensions
+    /// <summary>
+    /// Contains extension methods to register the <see cref="GraphSender"/> with the <c>FluentEmailServicesBuilder</c> from <c>FluentEmail.Core</c>.
+    /// </summary>
+    public static class FluentEmailServicesBuilderExtensions
     {
         public static FluentEmailServicesBuilder AddGraphSender(
             this FluentEmailServicesBuilder builder,
-            string GraphEmailAppId,
-            string GraphEmailTenantId,
-            string GraphEmailSecret,
+            GraphSenderOptions options)
+        {
+            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new GraphSender(options)));
+            return builder;
+        }
+
+        public static FluentEmailServicesBuilder AddGraphSender(
+            this FluentEmailServicesBuilder builder,
+            string graphEmailClientId,
+            string graphEmailTenantId,
+            string graphEmailSecret,
             bool saveSentItems = false)
         {
-            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new GraphSender(GraphEmailAppId, GraphEmailTenantId, GraphEmailSecret, saveSentItems)));
-            return builder;
+            var options = new GraphSenderOptions
+            {
+                ClientId = graphEmailClientId,
+                TenantId = graphEmailTenantId,
+                Secret = graphEmailSecret,
+                SaveSentItems = saveSentItems,
+            };
+            return builder.AddGraphSender(options);
         }
     }
 }
