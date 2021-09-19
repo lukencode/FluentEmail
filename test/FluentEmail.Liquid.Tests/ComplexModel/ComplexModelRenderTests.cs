e10a74b31f04b7e6123c11f66ae23d6ed363bea5
Jason Finch <jason.finch@gmail.com>
2021-09-19 21:47:18 +1000

    feat (Liquid): Add TemplateOptions as a configurable property.
    Added test for complex object.

---
+++ test/FluentEmail.Liquid.Tests/ComplexModel/ComplexModelRenderTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using FluentEmail.Core;
+using Fluid;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+
+namespace FluentEmail.Liquid.Tests.ComplexModel
+{
+    public class ComplexModelRenderTests
+    {
+        public ComplexModelRenderTests()
+        {
+            SetupRenderer();
+        }
+
+        [Test]
+        public void Can_Render_Complex_Model_Properties()
+        {
+            var model = new ParentModel
+            {
+                ParentName = new NameDetails { Firstname = "Luke", Surname = "Dinosaur" },
+                ChildrenNames = new List<NameDetails>
+                {
+                    new NameDetails { Firstname = "ChildFirstA", Surname = "ChildLastA" },
+                    new NameDetails { Firstname = "ChildFirstB", Surname = "ChildLastB" }
+                }
+            };
+
+            var expected = @"
+Parent: Luke
+Children:
+
+* ChildFirstA ChildLastA
+* ChildFirstB ChildLastB
+";
+
+            var email = Email
+                .From(TestData.FromEmail)
+                .To(TestData.ToEmail)
+                .Subject(TestData.Subject)
+                .UsingTemplate(Template(), model);
+            email.Data.Body.Should().Be(expected);
+        }
+
+        private string Template()
+        {
+            return @"
+Parent: {{ ParentName.Firstname }}
+Children:
+{% for Child in ChildrenNames %}
+* {{ Child.Firstname }} {{ Child.Surname }}{% endfor %}
+";
+        }
+
+        private static void SetupRenderer(
+            IFileProvider fileProvider = null,
+            Action<TemplateContext, object> configureTemplateContext = null)
+        {
+            var options = new LiquidRendererOptions
+            {
+                FileProvider = fileProvider,
+                ConfigureTemplateContext = configureTemplateContext,
+                TemplateOptions = new TemplateOptions { MemberAccessStrategy = new UnsafeMemberAccessStrategy() }
+            };
+            Email.DefaultRenderer = new LiquidRenderer(Options.Create(options));
+        }
+    }
+}
---
+++ test/FluentEmail.Liquid.Tests/ComplexModel/ParentModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace FluentEmail.Liquid.Tests
+{
+    public class ParentModel
+    {
+        public string Id { get; set; }
+        public NameDetails ParentName { get; set; }
+        public List<NameDetails> ChildrenNames { get; set; } = new List<NameDetails>();
+    }
+
+    public class NameDetails
+    {
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+    }
+}
