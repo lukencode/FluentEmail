c22f43ed90e509e01d7bd1dd0bdd0fc3e5244394
james.z <james.zheng@sjfood.com>
2022-12-28 05:27:43 +0000

    fix ReplaceRenderer class. Method Parse, NullReferenceException

---
+++ test/FluentEmail.Core.Tests/ReplaceRendererTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentEmail.Core.Defaults;
+using FluentEmail.Core.Interfaces;
+using FluentEmail.Core.Models;
+using NUnit.Framework;
+
+namespace FluentEmail.Core.Tests
+{
+    [TestFixture]
+    public class ReplaceRendererTest
+    {
+        [Test]
+        public void ModelPropertyValueIsNull_Test()
+        {
+            ITemplateRenderer templateRenderer = new ReplaceRenderer();
+
+            var address = new Address("james@test.com", "james");
+            Assert.True(address.Name == "james");
+            var template = "this is name: ##Name##";
+            Assert.True("this is name: james" == templateRenderer.Parse(template, address));
+
+            address.Name = null;
+            Assert.True("this is name: " == templateRenderer.Parse(template, address));
+        }
+    }
+}
