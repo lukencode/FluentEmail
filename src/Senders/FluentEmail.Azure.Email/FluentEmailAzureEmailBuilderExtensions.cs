e29963ad9708c0c74de35bdd97ff3209641c5896
John Campion Jr <john@brightshore.com>
2023-01-29 18:08:41 -0500

    Add azure email sender Mailkit PR 354 (#5)
    * Add Azure Email Communication Services Sender
    
    * Update FluentEmail.Azure.Email.csproj
    
    * Update AzureEmailSenderTests.cs
    
    * Remove unnecessary using statement
    
    * Update AzureEmailSender.cs comments
    
    * Update AzureEmailSender.cs comments
    
    ---------
    
    Co-authored-by: Eirik Sjøløkken <e.sjoeloe@gmail.com>

---
+++ src/Senders/FluentEmail.Azure.Email/FluentEmailAzureEmailBuilderExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Azure;
+using Azure.Communication.Email;
+using Azure.Core;
+using FluentEmail.Core.Interfaces;
+using FluentEmail.Azure.Email;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class FluentEmailAzureEmailBuilderExtensions
+    {
+        public static FluentEmailServicesBuilder AddAzureEmailSender(
+            this FluentEmailServicesBuilder builder,
+            string connectionString)
+        {
+            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new AzureEmailSender(connectionString)));
+            return builder;
+        }
+        
+        public static FluentEmailServicesBuilder AddAzureEmailSender(
+            this FluentEmailServicesBuilder builder,
+            string connectionString,
+            EmailClientOptions options)
+        {
+            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new AzureEmailSender(connectionString, options)));
+            return builder;
+        }
+        
+        public static FluentEmailServicesBuilder AddAzureEmailSender(
+            this FluentEmailServicesBuilder builder,
+            Uri endpoint,
+            AzureKeyCredential keyCredential,
+            EmailClientOptions options = default)
+        {
+            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new AzureEmailSender(endpoint, keyCredential, options)));
+            return builder;
+        }
+        
+        public static FluentEmailServicesBuilder AddAzureEmailSender(
+            this FluentEmailServicesBuilder builder,
+            Uri endpoint,
+            TokenCredential tokenCredential,
+            EmailClientOptions options = default)
+        {
+            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new AzureEmailSender(endpoint, tokenCredential, options)));
+            return builder;
+        }
+    }
+}
